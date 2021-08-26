19ff49367a668337ddf162169090a0399a5683ca
Leonardo Souza <leonardo030605@gmail.com>
2021-08-25 23:19:30 -0300

    Finalização no Bnco de Dados

--- Scripts/senai_spmedgroup_03_DQL.sql
+++ Scripts/senai_spmedgroup_03_DQL.sql
@@ -13,5 +13,31 @@ Select Count(Id_Prontuario)
 From Pacientes;
 Go
 
-Select Convert(Varchar(10), Data_Nascimento, 3)
+Select Convert(Varchar(10), Data_Nascimento, 103)
 From Pacientes
+
+Select Datediff(Year, Data_Nascimento, Getdate()) 
+From Pacientes;
+Go
+
+Create Function MedicoEspecialidade(@IdEspecialidade int)-- Função para calcular quantos médicos tem em cada especialide
+Returns Int
+As
+Begin
+	Declare @Quantidade int
+	Set @Quantidade =(Select Count(@IdEspecialidade) From Medico Where Id_Especialidade = @IdEspecialidade)
+	Return @Quantidade
+End
+Go
+
+Select dbo.MedicoEspecialidade(16);
+Go
+
+Create Procedure Busca_Usuario -- Procedure que retorna o nome do paciente a data de nascimento e a idade
+@Nome_Usuario Varchar (50)
+As
+	Select Id_Prontuario, Nome, Convert(Varchar(10), Data_Nascimento, 103) As 'Data de Nascimento', Datediff(Year, Data_Nascimento, Getdate()) As 'Idade'
+	From Pacientes
+	Where Nome = @Nome_Usuario
+
+Execute Busca_Usuario 'Henrique'
