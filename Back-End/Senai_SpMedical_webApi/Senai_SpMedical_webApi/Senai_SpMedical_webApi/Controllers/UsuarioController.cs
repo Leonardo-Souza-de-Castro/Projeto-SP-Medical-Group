b24666c2d9af37486351ed1e051df447d9aff4dd
Leonardo Souza de Castro <lleoanrdo030605@gmail.com>
2021-09-29 11:19:32 -0300

    Finalização do Controller de Usuario

--- Back-End/Senai_SpMedical_webApi/Senai_SpMedical_webApi/Senai_SpMedical_webApi/Controllers/UsuarioController.cs
+++ Back-End/Senai_SpMedical_webApi/Senai_SpMedical_webApi/Senai_SpMedical_webApi/Controllers/UsuarioController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Senai_SpMedical_webApi.Domains;
 using Senai_SpMedical_webApi.Interfaces;
 using Senai_SpMedical_webApi.Repositories;
 using System;
@@ -21,6 +22,62 @@ namespace Senai_SpMedical_webApi.Controllers
             _UsuarioRepository = new UsuarioRepository();
         }
 
+        /// <summary>
+        /// Método responsavel por listar todos os usuarios
+        /// </summary>
+        /// <returns>Uma lista de usuarios</returns>
+        [HttpGet]
+        public IActionResult Listar()
+        {
+            return Ok(_UsuarioRepository.Listar());
+        }
 
+        /// <summary>
+        /// Método responsavel por buscar um usuario por um id especifico
+        /// </summary>
+        /// <param name="id">Id do usuario a ser buscado</param>
+        /// <returns>Usuario encontrado</returns>
+        [HttpGet("{id}")]
+        public IActionResult Buscar(int id)
+        {
+            return Ok(_UsuarioRepository.Buscar(id));
+        }
+
+        /// <summary>
+        /// Método responsavel por cadastrar um novo usuario
+        /// </summary>
+        /// <param name="UsuarioNovo">Objeto do tipo usuario a ser cadastrado</param>
+        [HttpPost]
+        public IActionResult Cadastrar(Usuario UsuarioNovo)
+        {
+            _UsuarioRepository.Cadastrar(UsuarioNovo);
+
+            return StatusCode(201);
+        }
+
+        /// <summary>
+        /// Método responsavel pela atualização de algum cadastro de usuario
+        /// </summary>
+        /// <param name="UsuarioAtualizado">Dados Atualizados</param>
+        /// <param name="id">Id do usuario a ser atualizado</param>
+        [HttpPut("{id}")]
+        public IActionResult Atualizar(Usuario UsuarioAtualizado, int id)
+        {
+            _UsuarioRepository.Atualizar(UsuarioAtualizado, id);
+
+            return StatusCode(201);
+        }
+
+        /// <summary>
+        /// Método responsavel por deletar um usuario
+        /// </summary>
+        /// <param name="id">Id do usuario a ser deletado</param>
+        [HttpDelete("{id}")]
+        public IActionResult Deletar(int id)
+        {
+            _UsuarioRepository.Deletar(id);
+
+            return StatusCode(203);
+        }
     }
 }
